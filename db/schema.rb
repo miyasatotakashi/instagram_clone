4e1e3a789a0385dee3fe04ab1a090e2d13361660
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-14 14:08:28 +0900

    user model 作成

---
+++ db/schema.rb
@@ -0,0 +1,14 @@
+ActiveRecord::Schema.define(version: 2022_10_14_045814) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
