741f012ff728421f394a4ff93a020543b030afaf
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-14 14:26:43 +0900

    セキュアパスワードの設定

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,5 @@
-ActiveRecord::Schema.define(version: 2022_10_14_045814) do
+ActiveRecord::Schema.define(version: 2022_10_14_052022) do
 
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
@@ -9,6 +8,7 @@ ActiveRecord::Schema.define(version: 2022_10_14_045814) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
