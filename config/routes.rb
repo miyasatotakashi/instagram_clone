3abc838bed9198dd21e04079b50b24a0db9212da
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-16 13:11:43 +0900

    画像アップロード機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root "users#new"
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :feeds do
+    collection do
+      post :confirm
+    end
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,14 @@
-ActiveRecord::Schema.define(version: 2022_10_14_052022) do
+ActiveRecord::Schema.define(version: 2022_10_16_035834) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "feeds", force: :cascade do |t|
+    t.text "image"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
