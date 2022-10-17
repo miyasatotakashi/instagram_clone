29d09a9df62011561a6acd430b65d9315652a252
takashi miyasato <on.the.line.mt215@gmail.com>
2022-10-18 00:04:19 +0900

    画像投稿アクセス制限実装

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,13 +1,12 @@
 class FeedsController < ApplicationController
+  before_action :set_feed, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
-  # GET /feeds or /feeds.json
   def index
     @feeds = Feed.all
   end
 
-  # GET /feeds/1 or /feeds/1.json
   def show
-    @feed = Feed.find(params[:id])
   end
 
   # GET /feeds/new
@@ -19,35 +18,24 @@ class FeedsController < ApplicationController
     end
   end
 
-  # GET /feeds/1/edit
   def edit
-    @feed = Feed.find(params[:id])
-    if current_user != user
-      redirect_to feeds_path(current_user)
-    end
   end
 
-  # POST /feeds or /feeds.json
   def create
     @feed = Feed.new(feed_params)
-
-    respond_to do |format|
+    if params[:back]
+      render :new
+    else
       if @feed.save
-        format.html { redirect_to feed_url(@feed), notice: "Feed was successfully created." }
-        format.json { render :show, status: :created, location: @feed }
+        redirect_to feeds_path, notice: "投稿しました！"
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
+        render :new
       end
     end
   end
 
   # PATCH/PUT /feeds/1 or /feeds/1.json
   def update
-    @feed = Feed.find(params[:id])
-    if current_user != user
-      redirect_to feeds_path(current_user)
-    end
     respond_to do |format|
       if @feed.update(feed_params)
         format.html { redirect_to feed_url(@feed), notice: "Feed was successfully updated." }
@@ -61,7 +49,6 @@ class FeedsController < ApplicationController
 
   # DELETE /feeds/1 or /feeds/1.json
   def destroy
-    @feed = Feed.find(params[:id])
     @feed.destroy
 
     respond_to do |format|
@@ -72,17 +59,26 @@ class FeedsController < ApplicationController
 
   def confirm
     @feed = Feed.new(feed_params)
+    @feed.user_id = current_user.id
     render :new if @feed.invalid?
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_feed
-      @feed = Feed.find(params[:id])
-    end
+  def set_feed
+    @feed = Feed.find(params[:id])
+  end
 
     # Only allow a list of trusted parameters through.
-    def feed_params
-      params.require(:feed).permit(:image, :image_cache, :content)
+  def feed_params
+    params.require(:feed).permit(:image, :image_cache, :content)
+  end
+
+  def correct_user
+    @feed = Feed.find(params[:id])
+    if @feed.user_id != current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to feeds_path
     end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,13 @@ Bundler.require(*Rails.groups)
 
 module InstagramClone
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
   end
 end
